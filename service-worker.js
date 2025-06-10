b2cfaa4231beafc4ccd490157df59ac21a738f39
odangdi <dangdi@gmail.com>
2025-06-10 17:51:36 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const cacheName = 'simulador-rt2026-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
